bb2a7c7fd494fe75f852720fc384c9d220f809e1
ahamidovic2 <ahamidovic2@etf.unsa.ba>
2020-10-29 19:05:58 +0100

    uradjen zadatak

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,8 +1,21 @@
 package ba.unsa.etf.rpr;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+public static int sumaCifara (int n){
+    int suma=0;
+    while(n!=0){
+        suma+=n%10;
+        n/=10;
+    }
+    return suma;
+}
     public static void main(String[] args) {
-        System.out.println("Cmon everybody");
+        Scanner scanner = new Scanner(System.in);
+        int x= scanner.nextInt();
+        for(int i=1; i<x; i++){
+            if(i%sumaCifara(i)==0) System.out.print(i + " ");
+        }
     }
 }
